8c2082da960b1fafacad82156600af38a4da50a1
Николай Зайцев <nzaitsev11@gmail.com>
2022-05-22 11:12:57 +0200

    Dup prints

--- Finding_Duplicate_Lines/main.go
+++ Finding_Duplicate_Lines/main.go
@@ -1,7 +1,23 @@
+// Dup1 prints the text of each line that appears more than
+// once in the standard input, preceded by its count.
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hi")
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
