653c8dc740eee4fa551c856ca02909dbbe1b6715
Николай Зайцев <nzaitsev11@gmail.com>
2022-05-31 12:42:06 +0200

    check card with the function

--- card_check/check_card.go
+++ card_check/check_card.go
@@ -2,21 +2,43 @@ package main
 
 import "fmt"
 
-func main() {
+func read() (int, string) {
+	var x string
 	fmt.Println("Write number of card!")
-	var a string
-	fmt.Scan(&a)
-	n := len(a)
+	fmt.Scan(&x)
+	n := len(x)
+	return n, x
+}
+func check(n int, x string) bool {
+	if !f1(n) {
+		return false
+	}
+	if !f2(n, x) {
+		return false
+	}
+	if !f3(n, x) {
+		return false
+	}
+	fmt.Println("Your card have accepted!")
+	return true
+}
+func f1(n int) bool {
 	if n != 16 {
 		fmt.Println("Error: 01")
-		return
+		return false
 	}
+	return true
+}
+func f2(n int, a string) bool {
 	for i := 0; i < n; i++ {
 		if a[i] < '0' || a[i] > '9' {
 			fmt.Println("Error: 02")
-			return
+			return false
 		}
 	}
+	return true
+}
+func f3(n int, a string) bool {
 	sum := 0
 	x := 0
 	for i := n - 1; i >= 0; i-- {
@@ -35,8 +57,14 @@ func main() {
 	}
 	if sum%10 != 0 {
 		fmt.Println("Error: 03")
-		return
+		return false
 	}
-	fmt.Println("Your card have accepted!")
+	return true
+}
+func main() {
+	n, a := read()
+	check(n, a)
 	return
 }
+
+//5168441223630339
