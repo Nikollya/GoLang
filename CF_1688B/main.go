0325efc1bba3f11f29a98d34623ae59e8caadc84
Николай Зайцев <nzaitsev11@gmail.com>
2022-06-03 17:46:44 +0200

    CF

--- CF_1688B/main.go
+++ CF_1688B/main.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	var t int
+	in := bufio.NewReader(os.Stdin)
+	fmt.Fscan(in, &t)
+	for e := 0; e < t; e++ {
+		var n int
+		fmt.Fscan(in, &n)
+		a := make([]int, n)
+		f1, f2 := false, true
+		kn := 0
+		pos := 0
+		km := 0
+		for i := 0; i < n; i++ {
+			fmt.Fscan(in, &a[i])
+			if a[i]%2 != 0 {
+				f1 = true
+			}
+
+			if a[i]%2 == 0 {
+				p := a[i]
+				kl := 0
+				for u := 0; ; u++ {
+					kl++
+					p /= 2
+					if p%2 != 0 {
+						break
+					}
+				}
+				if f2 || (kl < km) {
+					pos = i
+					km = kl
+				}
+				f2 = false
+				kn++
+			}
+			//fmt.Println(km, "////*")
+		}
+		if f1 && f2 {
+			fmt.Println(0)
+		} else {
+			if f1 {
+				fmt.Println(kn)
+			} else {
+				k := 0
+				for {
+					a[pos] /= 2
+					k++
+					//fmt.Println(pos, "//")
+					if a[pos]%2 != 0 {
+						fmt.Println(k + kn - 1)
+						break
+					}
+				}
+			}
+		}
+
+	}
 }
